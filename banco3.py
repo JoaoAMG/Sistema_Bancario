89162fd0c9f9d3314c95dfdb4659c001aecfa7f7
joaoAMG <joaofelipegqs@gmail.com>
2024-05-25 19:54:01 -0300

    feat: Sistema totalmente refeito

--- banco3.py
+++ banco3.py
@@ -1,63 +1,62 @@
 class Conta:
-    def __init__(self,saldo,numero,agencia,cliente,historico):
+    def __init__(self, saldo, numero, agencia, cliente, historico):
         self.saldo = saldo
         self.numero = numero
         self.agencia = agencia
         self.cliente = cliente
         self.historico = historico
-        saldo = float
-        numero = int
-        agencia = str
-        cliente = Cliente
-        historico = Historico
-    def saldo(): 
-        float
-    def nova_conta(cliente,numero): 
-        Conta
-    def sacar(valor:float):
-        bool
-    def depositar(valor:float):
-        bool
 
+    def sacar(self, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+            return True
+        else:
+            return False
 
-class Conta_corrente(Conta):
-    def __init__(self,limite,limite_saques):
+    def depositar(self, valor):
+        self.saldo += valor
+        return True
+
+class ContaCorrente(Conta):
+    def __init__(self, saldo, numero, agencia, cliente, historico, limite, limite_saques):
+        super().__init__(saldo, numero, agencia, cliente, historico)
         self.limite = limite
         self.limite_saques = limite_saques
-        limite = float
-        limite_saques = int
 
 class Cliente:
-    def __init__(self,endereco,contas):
+    def __init__(self, endereco):
         self.endereco = endereco
-        self.contas = contas 
+        self.contas = []
 
-class Pessoa_fisica(Cliente):
-    def __init__(self,cpf,nome,data_nascimento):
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
+
+class PessoaFisica(Cliente):
+    def __init__(self, endereco, cpf, nome, data_nascimento):
+        super().__init__(endereco)
         self.cpf = cpf
         self.nome = nome
         self.data_nascimento = data_nascimento
 
-
-class Saque:
-    def __init__(self,valor):
+class Transacao:
+    def __init__(self, valor):
         self.valor = valor
-        valor = float
 
-class Deposito:
-    def __init__(self,valor):
-        self.valor = valor
-        valor = float
-
-class Transacao(Saque,Deposito):
-    def __init__(self,valor):
-        super().__init__(valor=valor)
-    
-    def Registrar_conta(conta:Conta):
+    def registrar_conta(self, conta):
         pass
 
-class Historico():
-    def adicionar_transacao(transacao):
-        pass
+class Historico:
+    def __init__(self):
+        self.transacoes = []
 
+    def adicionar_transacao(self, transacao):
+        self.transacoes.append(transacao)
 
+class Saque(Transacao):
+    def __init__(self, valor):
+        super().__init__(valor)
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        super().__init__(valor)
+# Aqui começa o menu Interativo
